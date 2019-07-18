1ac84ffa5325cea6ddefa10ea22da4edaa917177
HowarSTAR <sevagrigoryev@gmail.com>
2019-07-18 03:04:02 +0300

    Resize. Viewport.

---
+++ core/src/com/littlebiglion/screens/BaseScreen.java
@@ -0,0 +1,159 @@
+package com.littlebiglion.screens;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+public abstract class BaseScreen extends ApplicationAdapter implements Screen, InputProcessor {
+
+
+    protected SpriteBatch batch;
+    protected Rectangle playerOne;
+    protected Vector2 posi;
+    protected OrthographicCamera camera;
+    protected Vector2 v;
+    protected Array<Rectangle> daggerDrops;
+    protected Array<Rectangle> daggerDropsBack;
+    protected long lastDropTime;
+
+    Viewport viewport;
+
+    /**
+     * Задаем направление "прыжока" персонажа, а так же его скорость
+     */
+    protected int x = 0;
+    protected int y = 7;
+
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+
+        camera = new OrthographicCamera();
+
+        //  FitViewport –  поддерживает виртуальный размер экрана, всегда сохранять соотношение размера виртуального экрана
+        viewport = new FitViewport(1024,600,camera);
+        viewport.apply();
+        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+
+        Gdx.input.setInputProcessor(this);
+
+        posi = new Vector2();
+        v = new Vector2(x, y);
+
+        playerOne = new Rectangle();
+        posi.x = 700 / 2 - 74 / 2;
+        posi.y = 100;
+        playerOne.width = 74;
+        playerOne.height = 74;
+
+        daggerDropsBack = new Array<Rectangle>();
+        daggerDrops = new Array<Rectangle>();
+
+        spawnDagger();
+        spawnDaggerBack();
+    }
+
+    protected void spawnDagger(){
+        Rectangle daggerDrop = new Rectangle();
+        daggerDrop.x = 0;
+        daggerDrop.y = MathUtils.random(100, 480);
+        daggerDrop.width = 1024;
+        daggerDrop.height = 700;
+        daggerDrops.add(daggerDrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    protected void spawnDaggerBack(){
+        Rectangle daggerDropBack = new Rectangle();
+        daggerDropBack.x = 1024;
+        daggerDropBack.y = MathUtils.random(100, 480);
+        daggerDropBack.width = 1024;
+        daggerDropBack.height = 700;
+        daggerDropsBack.add(daggerDropBack);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+    }
+
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
--- desktop/src/com/littlebiglion/desktop/DesktopLauncher.java
+++ desktop/src/com/littlebiglion/desktop/DesktopLauncher.java
@@ -3,14 +3,12 @@ package com.littlebiglion.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.littlebiglion.DaggerJumper;
-import com.littlebiglion.DaggerJumperTwo;
-import com.littlebiglion.Screens;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 700;
-		config.height = 550;
-		new LwjglApplication(new Screens(), config);
+		config.width = 1024;
+		config.height = 700;
+		new LwjglApplication(new DaggerJumper(), config);
 	}
 }
